07f0281decb8c0fc8a45c594cc9307c358813b13
w1cerg <serg.wolf@mail.ru>
2014-06-30 00:17:10 +0600

    Update PHP.php

--- PHP.php
+++ PHP.php
@@ -5,3 +5,32 @@
 header('Content-Type: text/html; charset=utf-8');
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
+
+#
+# Функция для логирования скрипта
+#
+
+function script_end($error = false, $die = false){
+
+	global $log, $file_log;
+	
+	//Подключаем библиотеку для логов
+	require '../include/KLogger.php'; //https://github.com/katzgrau/KLogger
+	$loging = new KLogger ( $file_log , KLogger::INFO );
+
+	if($debug){
+		echo '<pre>';
+		echo $log;
+		echo '</pre>';
+	}
+
+	if(!$error){
+		$loging->logInfo($log); //действия при успешном выполнении
+	}
+	else{
+		$loging->logError($log); //действия при ошибке 
+	}
+	
+	if($die)
+		die();
+}
