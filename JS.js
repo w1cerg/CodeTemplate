c71b98cf939060e6286b0de9b778ca4b5ab44c56
w1cerg <serg.wolf@mail.ru>
2015-06-03 16:45:37 +0500

    function get delta date in sec,min,day,month,year

--- JS.js
+++ JS.js
@@ -2,3 +2,60 @@
 function MyPreventDefault(ev){
   if (ev.preventDefault) { ev.preventDefault(); } else { ev.returnValue = false; }
 }
+
+// get delta date in sec,min,day,month,year
+function getDeltaDate(from, to){
+    var dFrom = new Date(from);
+    var dTo = new Date(to);
+
+    var year = 0;
+    var month = 0;
+    var hours = 0;
+    var days = 0;
+    var minutes = 0;
+    var seconds = 0;
+
+    seconds = dTo.getSeconds() - dFrom.getSeconds();
+    if( seconds < 0 ){
+        seconds += 60;
+        minutes--;
+    }
+    minutes += dTo.getMinutes() - dFrom.getMinutes();
+    if( minutes < 0 ){
+        minutes += 60;
+        hours--;
+    }
+    hours += dTo.getHours() - dFrom.getHours();
+    if( hours < 0 ){
+        hours += 24;
+        days--;
+    }
+    days += dTo.getDate() - dFrom.getDate();
+    if( days < 0 ){
+        days += 30; //посчитать количество дней в месяце From
+        month--;
+    }
+    month += dTo.getMonth() - dFrom.getMonth();
+    if( month < 0 ){
+        month += 24;
+        year--;
+    }
+    year += dTo.getFullYear() - dFrom.getFullYear();
+    
+    var res =
+        (
+            (year > 0)?
+                year+( (year>4)?'л ':'г ' )
+                :''
+        ) + (
+            (month > 0)?
+                month+'м '
+                :''
+        ) + (
+            (days > 0)?
+                days+'д '
+                :''
+        ) + hours + ':' + minutes;
+
+    return res;
+}
