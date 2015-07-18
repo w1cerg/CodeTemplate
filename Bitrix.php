72cc6be4b23183bde7c1601cc6ac18f361825430
w1cerg <serg.wolf@mail.ru>
2015-07-18 14:31:22 +0500

    регистрация, только email

--- Bitrix.php
+++ Bitrix.php
@@ -20,3 +20,24 @@ $arUserFields = $GLOBALS["USER_FIELD_MANAGER"]->GetUserFields("IBLOCK_2_SECTION"
 foreach ($arUserFields as $FIELD_NAME => $arUserField){
  	test_dump($arUserField);
 }
+
+/*
+ * Логин выставляем равный email
+ */ 
+AddEventHandler("main", "OnBeforeUserRegister", "OnBeforeUserUpdateHandler");
+AddEventHandler("main", "OnBeforeUserUpdate", "OnBeforeUserUpdateHandler");
+function OnBeforeUserUpdateHandler(&$arFields)
+{
+	$arFields["LOGIN"] = $arFields["EMAIL"];
+	return $arFields;
+}
+
+/*
+ * Шаблон регистрации, убираем предупреждение про логин
+ */
+
+foreach ($arResult["ERRORS"] as $key => $error){
+	$error = preg_replace("/Пользователь с логином.*?уже существует.<br>/", "", $error);
+	$arResult["ERRORS"][$key] = $error;
+	// ..
+}
